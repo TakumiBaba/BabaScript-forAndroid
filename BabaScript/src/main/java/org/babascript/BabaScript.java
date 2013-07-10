b9d3f78fea5bd292dce2111c57515c0015ef8f2c
TakumiBaba <s09704tb@gmail.com>
2013-07-11 03:07:28 +0900

    return value できるようにした

--- BabaScript/src/main/java/org/babascript/BabaScript.java
+++ BabaScript/src/main/java/org/babascript/BabaScript.java
@@ -1,26 +1,36 @@
 package org.babascript;
 
+import android.app.Activity;
 import android.util.Log;
+import android.widget.ListView;
 
 import com.takumibaba.lindaclient.Linda;
 import com.takumibaba.lindaclient.LindaCallback;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.util.logging.Handler;
 
 /**
  * Created by s09704tb on 2013/07/01.
  */
 public class BabaScript implements LindaCallback {
-
+    private MainActivity mActivity;
     private Linda linda;
-    private Linda.TupleSpace ts;
+    public Linda.TupleSpace ts;
+    public ListView listView;
+    public Handler mHandler;
 
-    public BabaScript(){
+    public BabaScript(Activity activity){
         linda = new Linda();
         ts = linda.ts;
         linda.callback = this;
+        mActivity = (MainActivity)activity;
         linda.connect();
+        listView = (ListView)activity.findViewById(R.id.listView);
+
     }
 
     @Override
@@ -28,13 +38,29 @@ public class BabaScript implements LindaCallback {
         Log.d("BB:onConnect", json.toString());
         JSONArray tuple = new JSONArray();
         tuple.put("babascript");
+        tuple.put("eval");
         ts.watch(tuple);
     }
 
     @Override
     public void watch(JSONObject json) {
         Log.d("BB:onWatch", json.toString());
-        
+        JSONArray tuple = null;
+        try {
+            tuple = json.getJSONArray("data");
+            final Order order = new Order();
+            order.content = tuple.getString(2);
+            order.callback = tuple.getJSONObject(4).getString("callback");
+            order.option = tuple.getJSONArray(3);
+            mActivity.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    mActivity.addOrder(order);
+                }
+            });
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
     @Override
@@ -51,4 +77,6 @@ public class BabaScript implements LindaCallback {
     public void take(JSONObject json) {
         Log.d("BB:onTake", json.toString());
     }
+
+
 }
--- BabaScript/src/main/java/org/babascript/MainActivity.java
+++ BabaScript/src/main/java/org/babascript/MainActivity.java
@@ -1,19 +1,45 @@
 package org.babascript;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import com.takumibaba.lindaclient.Linda;
 
+import org.json.JSONArray;
+
 public class MainActivity extends Activity {
     Linda linda;
+    public ListView listView;
+    public OrderArrayAdapter adapter;
+    private BabaScript bb;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        BabaScript bb = new BabaScript();
+        listView = (ListView)findViewById(R.id.listView);
+        adapter = new OrderArrayAdapter(this, android.R.layout.simple_list_item_1);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Order order = adapter.orders.get(i);
+                Intent intent = new Intent();
+                intent.setClassName(MainActivity.this, "org.babascript.OrderReply");
+                intent.putExtra("callback", order.callback);
+                intent.putExtra("content", order.content);
+                startActivityForResult(intent, 0);
+            }
+        });
+        bb = new BabaScript(this);
+
     }
 
 
@@ -23,5 +49,36 @@ public class MainActivity extends Activity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
-    
+
+    public void addOrder(Order order){
+        adapter.add(order);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent intent){
+        if(resultCode == RESULT_OK){
+            Bundle extras = intent.getExtras();
+            String callback = "";
+            String content = "";
+            if(extras != null){
+                callback = extras.getString("callback");
+                content = extras.getString("content");
+            }
+            for(int i=0;i<adapter.orders.size();i++){
+                if(adapter.orders.get(i).callback.equals(callback)){
+                    Log.d("RESULT", "delete!");
+                    adapter.remove(i);
+                    adapter.notifyDataSetChanged();
+                    JSONArray tuple = new JSONArray();
+                    tuple.put("babascript");
+                    tuple.put("return");
+                    tuple.put(callback);
+                    tuple.put(content);
+                    tuple.put(new JSONArray());
+                    bb.ts.write(tuple, new JSONArray());
+                }
+            }
+        }
+    }
+
 }
